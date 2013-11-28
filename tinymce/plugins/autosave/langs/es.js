a95f2437554b17aff616325a0dde4f1dbb6bb833
Adionne <git@adionne.com>
2013-11-28 14:14:37 -0500

    init repo

---
+++ tinymce/plugins/autosave/langs/es.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('es.autosave',{
+restore_content: "Restaurar guarda automáticamente el contenido",
+warning_message: "Si restaura el contenido guardado, se perderá todo el contenido que se encuentra actualmente en el editor. \n\nEstá seguro que desea restaurar el contenido guardado?"
+});
---
+++ tinymce/plugins/autosave/langs/fr.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('fr.autosave',{
+restore_content: "Restaurer sauvegarde automatique de contenu",
+warning_message: "Si vous restaurez le contenu enregistré, vous perdrez tout le contenu qui se trouve actuellement dans l'éditeur. \n\nÊtes-vous sûr de vouloir restaurer le contenu enregistré?"
+});
